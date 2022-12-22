2f3ac53829d765ca2cbe406176bd8206393ddf5f
caiquemartins.dev <caiquemartins.dev@gmail.com>
2022-12-22 18:52:41 -0300

    inicio soma mínima

---
+++ somaMinima-parMinimo.rb
@@ -0,0 +1,24 @@
+# Dada uma matriz A[] de n elementos. 
+# Precisamos selecionar dois elementos adjacentes e excluir o maior deles 
+# e armazenar o menor deles em outro array, digamos B[]. 
+# Precisamos realizar esta operação até que a matriz A[] contenha apenas um único elemento. 
+# Finalmente, temos que construir o array B[] de forma que a soma total de seus elementos seja mínima. 
+# Imprima a soma total da matriz B[]
+
+matrizA = [2,6,4,9,11,3,7,15]
+
+# Primeiro achar o menor número do array e seu índice
+
+def somaMinima (matrizA)
+    menorNumero = matrizA.min
+    indiceMenorNumero = matrizA.index(menorNumero)
+    matrizB = []
+
+
+    while matrizA.length > 1
+        if
+    end 
+    
+
+
+end
